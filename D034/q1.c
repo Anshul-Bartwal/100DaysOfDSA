d63b159ed78f9864cb4214a7cdb4497ac1ffba25
Anshul-Bartwal <anshulbartwal720@gmail.com>
2026-03-21 13:43:42 +0530

    Added Day 34

---
+++ D034/q1.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* top = NULL;
+
+// Push
+void push(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = top;
+    top = newNode;
+}
+
+// Pop
+int pop() {
+    if (top == NULL) {
+        printf("Stack Underflow\n");
+        return 0;
+    }
+    struct Node* temp = top;
+    int val = temp->data;
+    top = top->next;
+    free(temp);
+    return val;
+}
+
+int main() {
+    char exp[100];
+    int i = 0;
+
+    // Read full line including spaces
+    fgets(exp, sizeof(exp), stdin);
+
+    while (exp[i] != '\0') {
+
+        // Skip spaces AND newline
+        if (exp[i] == ' ' || exp[i] == '\n') {
+            i++;
+            continue;
+        }
+
+        if (isdigit(exp[i])) {
+            int num = 0;
+
+            while (isdigit(exp[i])) {
+                num = num * 10 + (exp[i] - '0');
+                i++;
+            }
+
+            push(num);
+        }
+        else {
+            int b = pop();
+            int a = pop();
+
+            switch (exp[i]) {
+                case '+': push(a + b); break;
+                case '-': push(a - b); break;
+                case '*': push(a * b); break;
+                case '/': push(a / b); break;
+            }
+            i++;
+        }
+    }
+    printf("%d\n", pop());
+
+    return 0;
+}
